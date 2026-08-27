844d615afc5d656d48be7418abf8c499ccfe01c8
rgabrielly2408-eng <rgabrielly2408@gmail.com>
2026-08-27 11:53:02 -0300

    Implementar classe abstrata Animal

--- modelos/animal.py
+++ modelos/animal.py
@@ -1,4 +1,19 @@
-class Animal:
-    pass
+from abc import ABC, abstractmethod
 
-Criar classe Animal
+
+class Animal(ABC):
+
+    def __init__(self, nome, idade, peso):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+
+    @abstractmethod
+    def emitir_som(self):
+        pass
+
+    @abstractmethod
+    def descricao(self):
+        pass
+
+Implementar classe abstrata Animal
